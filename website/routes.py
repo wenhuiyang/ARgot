066f34d76d456515b401a70416d073bb054a71ad
dchen7321 <dchen7321@gmail.com>
2017-11-11 14:22:36 -0500

    Added Google Translation API.

--- website/routes.py
+++ website/routes.py
@@ -7,11 +7,16 @@
 
 # if __name__ == '__main__':
 #     app.run()
-
+# -*- coding: utf-8 -*-
 import requests
 import json
+import os
 
 from flask import Flask, jsonify
+from google.cloud import translate
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./apiKey/ARgot-2901c81d1cd3.json"
 
 app_id = 'e299e0de'
 app_key = '3ac35902b6dcfa9e855f892dd0f68665'
@@ -31,14 +36,22 @@ def getTranslation(word):
 	r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
 	print("code {}\n".format(r.status_code))
 	#print("json \n" + json.dumps(r.json()))
+	translation = translate_text(word)
 	return jsonify({'message' : r.json()});
+
+
 
 @app.route('/favicon.ico', methods=['GET'])
 def favicon():
 	return jsonify({'message' : 'favicon'})
 
+def translate_text(text, target='es'):
+	translate_client = translate.Client()
+	result = translate_client.translate(text, target_language=target)
 
+	return result['translatedText']
 
  
 if __name__ == '__main__':
   app.run(debug=True, port=8080)
+
